dcc5676f95135f87537f6f24509dcc113295f4b2
thegoat <ruppatel11599@gmail.com>
2021-02-08 11:01:05 -0500

    works better

--- src/main/java/edu/ithaca/dragon/games/tictactoe/TicTacToeMain.java
+++ src/main/java/edu/ithaca/dragon/games/tictactoe/TicTacToeMain.java
@@ -1,12 +1,11 @@
 package edu.ithaca.dragon.games.tictactoe;
 
-import edu.ithaca.dragon.games.tictactoe.player.BrainDeadPlayer;
-import edu.ithaca.dragon.games.tictactoe.player.ExpertPlayer;
+import edu.ithaca.dragon.games.tictactoe.player.RuleBasedAgentRup;
 import edu.ithaca.dragon.games.tictactoe.player.HumanPlayer;
 
 public class TicTacToeMain {
     public static void main (String[] args){
-        new TicTacToeGame( new HumanPlayer(), new ExpertPlayer()).play();
+        new TicTacToeGame( new HumanPlayer(), new RuleBasedAgentRup()).play();
     }
     
 }
